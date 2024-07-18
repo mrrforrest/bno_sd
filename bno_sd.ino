56e6576ba5c28a1cc9f5968a62f58b75e5b5cbc2
mrrforrest <47060001+mrrforrest@users.noreply.github.com>
2024-07-18 12:57:24 -0400

    Update bno_sd.ino
    Added vague idea of separate core usage

--- bno_sd.ino
+++ bno_sd.ino
@@ -1,17 +1,15 @@
-// To improve SDA-to-SCL setup time during clock-stretching cycles, try adding a 2.7K preset_pinullup to SDA and a 4.7K pullup to SCL (or similar values), and use short I2C wires (a few cm).
-// At 10ms rate, BNO08x outputs most reports in one burst, Gyr-Quat-Lac-Mag, however Acc is asynchronous and a few percent faster. Situation may vary with SENSOR_US and maximum sensor rates.
-
 // To Do: - check to make sure sensors are sampling properly all together (check times between report deliveries + delays when only using 1 sensor)
 //        - write code to interpret data quickly
 
 #include <Wire.h>
+#include <queue>
+#include <SPI.h>
+#include <SdFat.h>
 
 #define BNOs        7           // number of BNO08x breakouts connected via TCA9548 mux
 #define RESET_PIN   4           // BNO reset pin
-#define DEBUG       0
 
-// sampling time (us) 10000L : 100Hz, 20000L : 50Hz
-#define GYR_Ts      10000L      
+#define GYR_Ts      10000L      // sampling time (us) 10000L : 100Hz, 20000L : 50Hz    
 #define ACC_Ts      10000L      
 #define MAG_Ts      25000L  
 
@@ -25,17 +23,47 @@
 #define MAG_REPORT  0x03        // magneto report (6.5.16)
 #define TIME_REPORT 0xFB        // time report (7.2.1)
 
+#define FILE_BASE_NAME "Data"
+
+static struct Sensor_Buffer {
+    uint8_t time;                       // timestamp
+    uint8_t idx;                        // index, changes to sensor label when full
+    int16_t data[255];
+} __attribute__((packed));
+
+const uint8_t BASE_NAME_SIZE = sizeof(FILE_BASE_NAME) - 1;
+const uint8_t chipSelect = SS; // needed?
+
+uint8_t data[3];                // 2D array of booleans, 1 if data has been received for ACC/GYR/MAG and BNO index
+
+SdFat sd;
+SdFile file;
+Sensor_Buffer* bufID[BNOs][3];
+
+std::queue<Sensor_Buffer*> sdQ;
+std::queue<Sensor_Buffer*> readQ;
 
 void setup() {
   Serial.begin(SERIAL_BAUD);
   pinMode(RESET_PIN, OUTPUT);
   reset();
+  build_buff();
+  file_setup();
+  xTaskCreatePinnedToCore( sensor_read, "Task1", 10000, NULL, 0, &Task1, 0); // needs tuning for word allocation
 }
 
-void loop() {
-  for (uint8_t id = 0; id < BNOs; id++) {  // check for reports from all BNOs
-    if (id == 2){id = 7;}
-    check_report(id);
-    if (id == 7){id = 2;}
-  }
+static void sensor_read () {
+    while (1) {
+        for (uint8_t id = 0; id < BNOs; id++) {  // check for reports from all BNOs
+            if (id == 2){id = 7;}
+            check_report(id);
+            if (id == 7){id = 2;}
+        }
+    } 
+}
+
+void loop () {
+    if (sdQ::size() >= 16) {
+        logData();
+    }
 }
